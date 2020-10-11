3dbabaf7453bc58b6be1f1967527614e77279968
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-10-11 10:33:37 +0100

    Refactor build package as a function

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ TEX_PACKAGE_FILENAME = 'emojitex.sty'
 TEX_PACKAGE_PREAMBLE = 'preamble.tex'
 
 
-preamble_contents = pathlib.Path(TEX_PACKAGE_PREAMBLE).read_text()
-
-print('Creating TeX package... ', end='')
-
-with open(TEX_PACKAGE_FILENAME, mode='w') as fid:
-    fid.write(f'{preamble_contents}\n')
+def build_package(emoji_glyph_max_length=1):
+    buffer = []
+    preamble_contents = pathlib.Path(TEX_PACKAGE_PREAMBLE).read_text()
+    buffer.append(preamble_contents)
     for emoji_name, emoji_glyph in emoji.unicode_codes.EMOJI_UNICODE.items():
         emoji_name = emoji_name.strip(':')
         # newunicodechar only admits single Unicode character
         # https://ctan.math.illinois.edu/macros/latex/contrib/newunicodechar/newunicodechar.pdf
-        if len(emoji_glyph) == 1:
-            fid.write(
+        if len(emoji_glyph) <= emoji_glyph_max_length:
+            buffer.append(
                 r'\newunicodechar{'
                 f'{emoji_glyph}'
                 r'}{{\NotoEmoji '
                 f'{emoji_glyph}'
                 r'}}'
                 f'  % {emoji_name}'
-                '\n'
             )
+    return '\n'.join(buffer)
 
+
+print('Creating TeX package... ', end='')
+package = build_package()
+pathlib.Path(TEX_PACKAGE_FILENAME).write_text(package)
 print(f'✅ Done! => {TEX_PACKAGE_FILENAME}')
